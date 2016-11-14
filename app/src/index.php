69061705ed6f7fefefb7438e14199f603af8b06a
Michael Ludvig <mludvig@logix.net.nz>
2016-11-15 11:46:15 +1300

    Updated logo in App

--- app/src/index.php
+++ app/src/index.php
@@ -1,4 +1,6 @@
 <?php
+# Disable caching to demonstrate that we've got multiple instances
+# with simlple webpage reload.
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
@@ -9,9 +11,13 @@ header("Expires: 0"); // Proxies.
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-<h1><?php echo "AWS AutoScaling Demo"; ?></h1>
+<img src="enterpriseIT-logo.png" alt="enterpriseIT logo"/>
+<h1><?php
+# This is a PHP code to test if PHP works :)
+echo "AWS AutoScaling Demo";
+?></h1>
 <p>
-<?php if($_SERVER["SERVER_ADDR"]) {?><h3>My IP is <?php echo $_SERVER["SERVER_ADDR"]; ?></h3><?php } ?>
+<?php if($_SERVER["SERVER_ADDR"]) {?><h3>Instance IP is <?php echo $_SERVER["SERVER_ADDR"]; ?></h3><?php } ?>
 </p>
 <p><a href="resize.php?image=earth-elevation.jpg&width=3000"/>Click to view a resized image</a><br/>
 (intentionally puts heavy load on the server to trigger autoscaling)</p>
